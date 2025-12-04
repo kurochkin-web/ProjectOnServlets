e3369668007a9039609b042678a772305309abd1
kurochkin-web <mr.bigdaddytom@mail.ru>
2025-12-04 21:06:34 +0300

    new start

---
+++ src/main/java/entity/Category.java
@@ -0,0 +1,19 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    private UUID id;
+    private String name;
+    private String description;
+}
